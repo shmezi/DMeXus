3f3535f351d8668f1ce4808238b1ca3935575990
shmezi <alexirving992@gmail.com>
2024-08-06 17:22:07 +0300

    Complete restructure!

---
+++ src/main/kotlin/lol/ezra/plugins/Routing.kt
@@ -0,0 +1,13 @@
+package lol.ezra.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+   routing {
+      get("/") {
+         call.respondText("Hello World!")
+      }
+   }
+}
